cb5d05401faf28c85869fea5f90fa698b6cd9422
Dave Lively <davelively14@gmail.com>
2017-11-23 07:01:33 -0500

    Built Blitzy worker

--- lib/blitzy.ex
+++ lib/blitzy.ex
@@ -1,18 +1,24 @@
 defmodule Blitzy do
-  @moduledoc """
-  Documentation for Blitzy.
-  """
+  use Timex
+  require Logger
 
-  @doc """
-  Hello world.
+  def start(url) do
+    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
+    handle_response({Duration.to_milliseconds(timestamp), response})
+  end
 
-  ## Examples
+  defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}}) when code >= 200 and code <= 304 do
+    Logger.info "worker [#{node()}-#{inspect self()}] completed in #{msecs}"
+    {:ok, msecs}
+  end
 
-      iex> Blitzy.hello
-      :world
+  defp handle_response({_msecs, {:error, reason}}) do
+    Logger.info "worker [#{node()}-#{inspect self()}] error due to #{inspect reason}"
+    {:error, reason}
+  end
 
-  """
-  def hello do
-    :world
+  defp handle_response({_msecs, _}) do
+    Logger.info "worker [#{node()}-#{inspect self()}] errored out"
+    {:error, :unknown}
   end
 end
