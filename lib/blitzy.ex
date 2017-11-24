d744cd8d006c11fa930b152f69389ffe1fe8d698
Dave Lively <davelively14@gmail.com>
2017-11-24 06:41:51 -0500

    Added option func parameter for start
    Per the book's instruction, although the original text we're copying
    from does not have the optional func parameter.

--- lib/blitzy.ex
+++ lib/blitzy.ex
@@ -2,8 +2,8 @@ defmodule Blitzy do
   use Timex
   require Logger
 
-  def start(url) do
-    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
+  def start(url, func \\ &HTTPoison.get/1) do
+    {timestamp, response} = Duration.measure(fn -> func.(url) end)
     handle_response({Duration.to_milliseconds(timestamp), response})
   end
 
